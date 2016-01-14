4896b5bdb09f4c73d15f2a4ffb034ef7669a6597
ChristianneYu <yuchristianne@gmail.com>
2016-01-14 16:54:56 +0800

    initialized the string arrays. Title, description and genre at arrays.xml

--- app/src/main/java/sidespell/tech/viewholderquiz/fragments/ListViewFragment.java
+++ app/src/main/java/sidespell/tech/viewholderquiz/fragments/ListViewFragment.java
@@ -1,10 +1,12 @@
 package sidespell.tech.viewholderquiz.fragments;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import sidespell.tech.viewholderquiz.R;
 
@@ -13,13 +15,28 @@ import sidespell.tech.viewholderquiz.R;
  */
 public class ListViewFragment extends Fragment {
 
+
+    private ListView mListView;
+
     public static ListViewFragment newInstance() {
+
+
         return new ListViewFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_listview, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_listview, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+     mListView = (ListView) view.findViewById(R.id.listView);
+
     }
 }
